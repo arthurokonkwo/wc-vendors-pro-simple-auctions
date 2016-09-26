4f4855f9a60e877e180d06b2d76af52334f61828
digitalchild <digitalchild@gmail.com>
2016-09-26 18:33:09 +0700

    Preparing v1.0.4 for release

--- class-wcv-simple-auctions.php
+++ class-wcv-simple-auctions.php
@@ -3,7 +3,7 @@
  * Plugin Name: 	  WC Vendors Pro Simple Auctions
  * Plugin URI: 		  http://www.wcvendors.com/wc-vendors-simple-auctions/ 
  * Description: 	  Add WooCommerce simple auctions support to WC Vendors Pro 
- * Version:	 	  1.0.3
+ * Version:	 	  	  1.0.4
  * Author:            WC Vendors
  * Author URI:        http://www.wcvendors.com/
  * License:           GPL-2.0+
@@ -15,6 +15,22 @@
  * @since             1.0.1
  * @package           WCVendors_Pro_Simple_Auctions 
  *
+
+
+WC Vendors Simple Auctions is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+WC Vendors Simple Auctions is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with WC Vendors Simple Auctions. If not, see http://www.gnu.org/licenses/gpl-2.0.txt.
+
+
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
